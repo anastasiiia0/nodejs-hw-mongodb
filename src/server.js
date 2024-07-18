0f51bef99a75be5ffea3fb59831b7b769cfb114b
anastasiyaaa0 <anastasiiiia98@gmail.com>
2024-07-18 13:15:11 +0300

    add status property to '/contacts' route

--- src/server.js
+++ src/server.js
@@ -21,9 +21,11 @@ export const setupServer = () => {
 
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
-    res
-      .status(200)
-      .json({ message: 'Successfully found contacts!', data: contacts });
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
   });
 
   app.get('/contacts/:contactId', async (req, res) => {
@@ -36,9 +38,9 @@ export const setupServer = () => {
     }
 
     res.status(200).json({
-      data: contact,
       status: 200,
       message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
     });
   });
 
