2b64e5f414c9ce574f1ff144b57ef96326cc21f7
anastasiyaaa0 <anastasiiiia98@gmail.com>
2024-07-22 12:06:26 +0300

    fix createContactSchema & id parameter name

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,8 +3,8 @@ import Joi from 'joi';
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().min(3).max(20).required(),
-  isFavourite: Joi.boolean().required(),
+  email: Joi.string().email().min(3).max(20),
+  isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
 
--- src/validation/isValidId.js
+++ src/validation/isValidId.js
@@ -2,9 +2,9 @@ import { isValidObjectId } from 'mongoose';
 import { HttpError } from 'http-errors';
 
 export const isValidId = (req, res, next) => {
-  const { id } = req.params;
+  const { contactId } = req.params;
 
-  if (!isValidObjectId(id)) {
+  if (!isValidObjectId(contactId)) {
     throw HttpError(404, 'Not found');
   }
 
